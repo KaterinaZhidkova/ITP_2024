ab3628ec88dbaa71a0bc1fe10a566cc06ec093a0
KaterinaZhidkova <katerina.mutoy@gmail.com>
2024-11-08 00:17:50 +0300

    Create Display.java

---
+++ lab_11/Exercise_6/Display.java
@@ -0,0 +1,12 @@
+package lab_11.Exercise_6;
+import java.util.*;
+
+public class Display {
+    public void showBookDescription(String description) {
+        System.out.println("Description: ");
+        System.out.println(description);
+    }
+    public void showMessages(String message) {
+        System.out.println(message);
+    }
+}
